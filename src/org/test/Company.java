ea3537d6eef4218a9d38771f827601f7b60ac0f3
sarathy2511 <sarathy2511@gmail.com>
2020-11-24 22:03:56 +0530

    git over

---
+++ src/org/test/Company.java
@@ -0,0 +1,10 @@
+package org.test;
+
+public class Company extends Employee {
+	public static void main(String[] args) {
+		System.out.println(a);
+	empId();
+	
+	}
+
+}
---
+++ src/org/test/Employee.java
@@ -0,0 +1,9 @@
+package org.test;
+
+public class Employee {
+ static int a = 10;
+public static void empId() {
+	// TODO Auto-generated method stub
+System.out.println(a);
+}
+}
